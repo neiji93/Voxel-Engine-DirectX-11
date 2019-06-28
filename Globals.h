e2d7108b908658c7f3cb6fe1022fec14e8faee01
neiji93 <nejdet.altinisik@yahoo.fr>
2019-06-28 16:21:41 +0200

    Add files via upload

---
+++ Globals.h
@@ -0,0 +1,50 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+
+
+#include <D3D11.h>
+//#include <D3DX11.h>
+#include <D3Dcompiler.h>
+//#include <xnamath.h>
+
+
+//extern ID3D11Device* g_pd3dDevice;
+//extern ID3D11DeviceContext* g_pImmediateContext;
+
+
+
+
+//--------------------------------------------------------------------------------------
+// Helper for compiling shaders with D3DX11
+//--------------------------------------------------------------------------------------
+HRESULT CompileShaderFromFile( WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut );
+/*{
+    HRESULT hr = S_OK;
+
+    DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+    // Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+    // Setting this flag improves the shader debugging experience, but still allows 
+    // the shaders to be optimized and to run exactly the way they will run in 
+    // the release configuration of this program.
+    dwShaderFlags |= D3DCOMPILE_DEBUG;
+#endif
+
+    ID3DBlob* pErrorBlob;
+    hr = D3DX11CompileFromFile( szFileName, NULL, NULL, szEntryPoint, szShaderModel, 
+        dwShaderFlags, 0, NULL, ppBlobOut, &pErrorBlob, NULL );
+    if( FAILED(hr) )
+    {
+        if( pErrorBlob != NULL )
+            OutputDebugStringA( (char*)pErrorBlob->GetBufferPointer() );
+        if( pErrorBlob ) pErrorBlob->Release();
+        return hr;
+    }
+    if( pErrorBlob ) pErrorBlob->Release();
+
+    return S_OK;
+}*/
+
+
+#endif
